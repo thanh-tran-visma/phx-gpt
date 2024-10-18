a3b5f1dcd9a28e16fea411ca0494782e1eda737b
thanh-tran-visma <thanh.tran@visma.com>
2024-10-18 10:14:29 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -3,11 +3,10 @@ from app.api.router import router
 from app.llm.llm_model import BlueViGptModel
 from contextlib import asynccontextmanager
 
-blueViGpt = BlueViGptModel()
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup event
+    blueViGpt = BlueViGptModel()
     app.state.model = blueViGpt
     blueViGpt.load_model()
     yield
--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,5 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
+from unittest.mock import patch
 from app.main import app
 from app.api.http_status import HTTPStatus
 
@@ -9,30 +10,27 @@ def client():
         yield client
 
 def test_chat_endpoint_without_token(client):
+    # Test without token, should return 401 Unauthorized
     response = client.post("/chat/chat", json={"prompt": "test_message"})
-
+    
     # Assert the response status code is 401 (Unauthorized) because no token is provided
     assert response.status_code == HTTPStatus.UNAUTHORIZED.value
-
-    # Get the JSON response
+    
     json_response = response.json()
-
-    # Assert the presence of expected keys in the response
     assert "detail" in json_response
     assert json_response["detail"] == "Not authenticated"
 
-def test_chat_endpoint_with_token(client):
-    # Set the Bearer token in the headers
-    headers = {"Authorization": "Bearer 1234"}
+@patch("os.getenv", return_value="mocked_token")
+@patch("app.auth.auth.Auth.get_bearer_token", return_value="mocked_token")
+def test_chat_endpoint_with_token(mock_getenv, mock_auth, client):
+    # Set the mocked Bearer token in the headers
+    headers = {"Authorization": "Bearer mocked_token"}
     response = client.post("/chat/chat", json={"prompt": "test_message"}, headers=headers)
-
+    
     # Assert the response status code is 200 (OK) since the token is provided
     assert response.status_code == HTTPStatus.OK.value
-
-    # Get the JSON response
+    
     json_response = response.json()
-
-    # Assert the presence of expected keys in the response
     assert "response" in json_response
     assert isinstance(json_response["response"], str)
     assert json_response["response"] != ""
