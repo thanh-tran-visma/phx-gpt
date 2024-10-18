7b11ffba1250e116d97f7bb4decf56a10a3a8b1d
thanh-tran-visma <thanh.tran@visma.com>
2024-10-18 10:23:03 +0200

    fix: auth

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -6,6 +6,8 @@ class Auth:
     @staticmethod
     def get_bearer_token(request: Request):
         expected_token = os.getenv("BEARER_TOKEN")
+        
+        # Ensure the expected token exists
         if expected_token is None:
             raise HTTPException(
                 status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
@@ -14,6 +16,7 @@ class Auth:
         
         auth_header = request.headers.get("Authorization")
         
+        # If Authorization header is missing or not a Bearer token, raise 401 Unauthorized
         if not auth_header or not auth_header.startswith("Bearer "):
             raise HTTPException(
                 status_code=HTTPStatus.UNAUTHORIZED.value,
@@ -22,6 +25,7 @@ class Auth:
         
         token = auth_header.split("Bearer ")[1]
         
+        # If token does not match the expected token, raise 403 Forbidden
         if token != expected_token:
             raise HTTPException(
                 status_code=HTTPStatus.FORBIDDEN.value,
